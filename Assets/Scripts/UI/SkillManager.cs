a98ed29cd7ae7372e3ec3238c6c12ede86ee2cc1
chiakinanamiQW <148993154+chiakinanamiQW@users.noreply.github.com>
2024-01-21 13:08:17 +0800

    虚影UI制作

--- Assets/Scripts/UI/SkillManager.cs
+++ Assets/Scripts/UI/SkillManager.cs
@@ -1,17 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class SkillManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Image Skill1;
+    public Image Skill2;
+    public List<Sprite> sprites=new List<Sprite>();
+    public Dictionary<string,Sprite> imageDic=new Dictionary<string,Sprite>();
+    private void Awake()
     {
-        
+        imageDic["Cat"] = sprites[0];
+        imageDic["Rabbit"] = sprites[1];
+        imageDic["Squirrel"] = sprites[2];
+        imageDic["Bird"] = sprites[3];
     }
-
-    // Update is called once per frame
-    void Update()
+    private void UpdateSprites(string _name)
     {
         
     }
